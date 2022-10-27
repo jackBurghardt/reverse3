f49fcf780e9b8db60f474274d3c785578d83ab93
jackBurghardt <112026991+jackBurghardt@users.noreply.github.com>
2022-10-26 22:48:21 -0700

    Add files via upload

---
+++ Reverse3.java
@@ -0,0 +1,18 @@
+
+public class Reverse3 {
+	public int[] reverse3(int[] nums) {
+		  int[] in = new int[3];
+		  String s ="";
+		  int count = 0;
+		  for (int i = 2; i >= 0; i--){
+		    in[count] = nums[i];
+		    count++;
+		  }
+		  return in;
+		  
+		  
+		  
+		  //hhihihhh
+		}
+
+}
